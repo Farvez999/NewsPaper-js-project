cf04e62d24f0a9c978d2241c77488a2447696afb
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-09-03 15:17:57 +0600

    Spinner Added

--- js/app.js
+++ js/app.js
@@ -38,7 +38,21 @@ const setAllCategory = async () => {
 
 setAllCategory();
 
+// spinner
+const toggleSpinner = isLoading => {
+    const spinnerSection = document.getElementById('spinner');
+    if (isLoading) {
+        spinnerSection.classList.remove('d-none')
+    } else {
+        spinnerSection.classList.add('d-none')
+    }
+}
+
 const loadAllNews = async category_id => {
+
+    // spinner start
+    toggleSpinner(true);
+
     const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
     // console.log(category_id);
     const res = await fetch(url);
@@ -113,6 +127,9 @@ const displayNewItem = newsAll => {
         `;
         newsContainer.appendChild(div);
     })
+
+    //Spinner Stop
+    toggleSpinner(false);
 
 }
 
